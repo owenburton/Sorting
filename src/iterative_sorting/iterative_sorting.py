0f7c675f1a9ef16b849c4145d01c78fd2b8134ee
owenburton <owenburton@users.noreply.github.com>
2020-03-16 14:30:03 -0700

    finished bubble_sort v1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,5 +1,5 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(len(arr)-1):
         current_item = arr[i]
@@ -18,9 +18,31 @@ def selection_sort( arr ):
 
 
 # # TO-DO:  implement the Bubble Sort function below
-# def bubble_sort( arr ):
+def bubble_sort(arr):
+    if len(arr)>0:
+        condition_list = [False for i in arr]
+        condition_list[-1] = True
+        while False in condition_list:
+            # loop through each index in the array
+            for i in range(len(arr)-1):
+                # if the item on the right is greater
+                if arr[i+1] >= arr[i]:
+                    # set sorted = True
+                    condition_list[i] = True
+                # if the item on the right is less, 
+                else:
+                    # swap the current item and the one to the right
+                    current = arr[i]
+                    right = arr[i+1]
+                    arr[i+1] = current
+                    arr[i] = right 
+                    # if you swapped set sorted = false
+                    condition_list[i] = False
+                # print(arr)
+    return arr
 
-#     return arr
+# arr = [0,0,2,40,1,5,0,0,5,3,5,4,8,9]
+# print(bubble_sort(arr))
 
 
 # # STRETCH: implement the Count Sort function below
